a56a7950fc66c7c40e853023cfb4c9ce4826de73
baekho <klyhyeon@gmail.com>
2024-05-29 22:41:51 +0900

    feat init commit

---
+++ src/main/kotlin/hello/core/SpringCoreLectureApplication.kt
@@ -0,0 +1,11 @@
+package hello.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCoreLectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCoreLectureApplication>(*args)
+}
